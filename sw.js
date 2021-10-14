9e13a4465ba845d5e1d8c53c8512e1c665e5d852
gdemare <44768462+gdemare@users.noreply.github.com>
2021-10-14 14:10:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-function registerServiceWorker() {
-  // enregistre le script sw avec les navigateurs qui le gèrent
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
-      console.log('Service Worker enregistré correctement.');
-    }).catch(error => {
-      console.log('Erreur lors de l''enregistrement du Service Worker : ', error);
-    });
-  }
-}
+// sw.js
+self.addEventListener('install', e => {
+ e.waitUntil(
+   // Après l'installation du service worker,
+   // ouvre un nouveau cache
+   caches.open('mon-cache-pwa').then(cache => {
+     // Ajoute toutes les URLs des éléments à mettre en cache
+     return cache.addAll([
+       '/',
+       '/script.js',
+       '/style.css',
+       '/scienceDeLaVie.html',
+     ]);
+   })
+ );
+});
