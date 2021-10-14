b98884248cda421eaaea6b5b764db3c55804d0b5
gdemare <44768462+gdemare@users.noreply.github.com>
2021-10-14 14:26:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-// PWA
+//PWA
 function registerServiceWorker() {
-// enregistre le script sw avec les navigateurs qui le gèrent
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
-    console.log('Service Worker enregistré correctement.');
-  }).catch(error => {
-    console.log('Erreur lors de l''enregistrement du Service Worker : ', error);
-  });
+  // enregistre le script sw avec les navigateurs qui le gèrent
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
+      console.log('Service Worker enregistré correctement.');
+    }).catch(error => {
+      console.log('Erreur lors de l''enregistrement du Service Worker : ', error);
+    });
+  }
 }
-};
-  
+
+$(document).ready(function() {
   
 // animation menu déroulant
 var menu = ["s4", "s3", "s2", "s1", "campbell"];
