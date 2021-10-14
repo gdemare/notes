861f9f939a02d9eea841ed3925967078f1000b0c
gdemare <44768462+gdemare@users.noreply.github.com>
2021-10-14 14:11:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function registerServiceWorker() {
+  // enregistre le script sw avec les navigateurs qui le gèrent
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
+      console.log('Service Worker enregistré correctement.');
+    }).catch(error => {
+      console.log('Erreur lors de l''enregistrement du Service Worker : ', error);
+    });
+  }
+}
+
 $(document).ready(function() {
 // animation menu déroulant
 var menu = ["s4", "s3", "s2", "s1", "campbell"];
