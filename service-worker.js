1496142b065bf3fe3d8747cf95864f7915597c2a
gdemare <44768462+gdemare@users.noreply.github.com>
2021-10-15 14:38:34 +0200

    test pwa

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,19 @@
-// On écoute l'évènement install pour effectuer les actions de démarrage
-self.addEventListener("install", function(event) {
-  // Ici on utilise waitUntil qui attends une promesse pour valider l'installation
-  event.waitUntil(
-    caches.open("mon-site-cache-v1").then(function(cache) {
-      // On met en cache une liste d'URLs qui sont la fondation de notre app.
-      return cache.addAll([
-          "/",
-          "/style.css", 
-          "/script.js"
-      ]);
+var staticCacheName = "pwa";
+ 
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(["/"]);
     })
   );
 });
-
-// On indique que pour chaque requêtes, si cela match nos URLs de cache défini plus haut, alors on servira le cache plutôt que d'effectuer la vrai requête par le réseau.
-self.addEventListener('fetch', function(event) {
+ 
+self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
+ 
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache HIT, on retourne la réponse en cache.
-        if (response) {
-          return response;
-        }
-        
-        // Sinon on effectue la requête réellement et on retourne son contenu.
-        return fetch(event.request);
-      }
-    )
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
   );
 });
-
-// on vérifie que le navigateur est compatible avec service worker.
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      function(registration) {
-        console.log("Enregistrement OK - Scope : ", registration.scope);
-      },
-      function(err) {
-        console.log("Erreur d'enregistrement : ", err);
-      }
-    );
-  });
-}
